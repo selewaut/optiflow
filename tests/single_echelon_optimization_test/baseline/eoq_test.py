c72c3736117bfade78688cfc5fab8d08e1b407e2
selewaut <santielewaut@gmail.com>
2023-04-25 22:49:02 -0300

    refactor: eliminated unittest dependency for tests

--- tests/single_echelon_optimization_test/baseline/eoq_test.py
+++ tests/single_echelon_optimization_test/baseline/eoq_test.py
@@ -1,56 +1,28 @@
-import unittest
+import pytest
 from optiflow.baseline.single_echelon_models._eoq import EOQ, EOQProduction
 import numpy as np
 
 
-class EOQTest(unittest.TestCase):
-    # TODO migrate away from unittest
-    def setUp(self):
-        self.demand_forecasts = 600
-        holding_cost = 1.75
-        ordering_cost = 50
-        self.expected_order_quantity = 185.16401995451028
-        self.expected_cost = 324.037034920393
-        self.model = EOQ(holding_cost=holding_cost, ordering_cost=ordering_cost)
 
-    def test_calculate_order_quantities(self):
-        order_quantity, total_cost = self.model.calculate_order_quantities(
-            self.demand_forecasts
-        )
-        self.assertEqual(order_quantity, self.expected_order_quantity)
-
-    def test_calculate_total_cost(self):
-        print(self.demand_forecasts)
-        total_cost = self.model.total_cost(
-            self.demand_forecasts, Q=self.expected_order_quantity
-        )
-        self.assertEqual(total_cost, self.expected_cost)
+# EOQ Tests
+# Generate test using pytest.mark.parametrize and using as inputs: "demand_forecasts, holding_cost, ordering_cost, expected_order_quantity, expected_cost"
+# TODO: migrate away from unittest
 
 
-class EOQTestProduction(unittest.TestCase):
-    # TODO migrate away from unittest
-    def setUp(self):
-        self.demand_forecasts = 1300
-        self.expected_order_quantity = 626.8084945889684
-        self.expected_cost = 33.183979125298336
-        holding_cost = 0.225
-        ordering_cost = 8
-        production_rate = 1700
-        self.model = EOQProduction(
-            holding_cost=holding_cost,
-            ordering_cost=ordering_cost,
-            production_rate=production_rate,
-        )
 
-    def test_calculate_order_quantities(self):
-        order_quantity, total_cost = self.model.calculate_order_quantities(
-            self.demand_forecasts
-        )
-        self.assertEqual(order_quantity, self.expected_order_quantity)
 
-    def test_calculate_total_cost(self):
-        print(self.demand_forecasts)
-        total_cost = self.model.total_cost(
-            self.demand_forecasts, Q=self.expected_order_quantity
-        )
-        self.assertEqual(total_cost, self.expected_cost)
+@pytest.mark.parametrize("demand_forecasts, holding_cost, ordering_cost, expected_order_quantity, expected_cost", [(600, 1.75, 50, 185.16401995451028, 324.037034920393)])
+def test_calculate_order_quantities(demand_forecasts, holding_cost, ordering_cost, expected_order_quantity, expected_cost):
+    model = EOQ(holding_cost=holding_cost, ordering_cost=ordering_cost)
+    order_quantity, total_cost = model.calculate_order_quantities(demand_forecasts)
+    assert order_quantity == expected_order_quantity
+    assert total_cost == expected_cost
+
+@pytest.mark.parametrize("demand_forecasts, holding_cost, ordering_cost,production_rate, expected_order_quantity, expected_cost", [(1300, 0.225, 8,1700,  626.8084945889684, 33.183979125298336)])
+def test_calculate_order_quantities_production(demand_forecasts, holding_cost, ordering_cost,production_rate, expected_order_quantity, expected_cost):
+    model = EOQProduction(holding_cost=holding_cost, ordering_cost=ordering_cost, production_rate=production_rate)
+    order_quantity, total_cost = model.calculate_order_quantities(demand_forecasts)
+    assert order_quantity == expected_order_quantity
+    assert total_cost == expected_cost
+
+# Generate tests into a class to share inputs across different method tests, dont use unit test for this
