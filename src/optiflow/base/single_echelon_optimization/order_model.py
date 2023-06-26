225d0b876b8f622e469fc2142a804a11c87e135b
selewaut <santielewaut@gmail.com>
2023-06-26 00:40:56 -0300

    Updated base class for order model

--- src/optiflow/base/single_echelon_optimization/order_model.py
+++ src/optiflow/base/single_echelon_optimization/order_model.py
@@ -10,16 +10,13 @@ class OrderQuantityModel(ABC):
     """
 
     @abstractmethod
-    def calculate_order_quantities(self, demand_forecasts, inventory_levels):
+    def calculate_order_quantities(self, demand_forecasts):
         """Calculates the order quantities for the given demand forecasts and inventory levels.
 
         Parameters
         ----------
         demand_forecasts : array-like of shape (n_periods,)
             The demand forecasts for the next n periods.
-
-        inventory_levels : array-like of shape (n_periods,)
-            The inventory levels at the beginning of each of the next n periods.
 
         Returns
         -------
